892bf7ef7a7da79cab1d8bae743703883229a81a
森祐一朗 <apotikisin4632@gmail.com>
2023-05-20 21:19:24 +0900

    add edit page

--- django_app/views.py
+++ django_app/views.py
@@ -24,6 +24,8 @@ def home(request, user_name):
     }
     return render(request, 'home.html', context={'user_name': user_name, 'career': career})
 
+# ホーム画面
+
 
 def memo(request):
     memos = Memo.objects.all()
@@ -32,9 +34,32 @@ def memo(request):
     #     print(memo.memo)
     return render(request, 'memo.html', context={'memos': memos})
 
+# 新規メモ作成ページ
+
 
 def new_memo(request):
     new_memo = forms.MemoForm()
+    if request.method == 'POST':
+        new_memo = forms.MemoForm(request.POST)
+        print('--------- post --------------------')
+        print(new_memo)
+        # バリデーション実行後、保存
+        if new_memo.is_valid():
+            print('--------- validation and save ------------------')
+            new_memo.save()
+            # ホーム画面に遷移
+            return redirect('django_app:memo')
+
+    return render(request, 'new_memo.html', context={'new_memo': new_memo})
+
+# メモ編集ページ
+
+
+def edit_memo(request, memo_id):
+    content = Memo.objects.get(id=memo_id)
+    print("----- memo content -----")
+    print(content.memo)
+    new_memo = forms.MemoForm(initial={'memo': content.memo})
     if request.method == 'POST':
         new_memo = forms.MemoForm(request.POST)
         print('--------- post --------------------')
